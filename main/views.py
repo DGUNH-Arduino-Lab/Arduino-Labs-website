2edc8b6474c9fe4b09965fc3f3dca20d5e3dea8c
zhigibig <gamzat0004@mail.ru>
2023-04-07 16:02:35 +0300

    main app update

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return render(request, '')
+
+def projects(request):
+    return HttpResponse("<h1>List of projects</h1>")
